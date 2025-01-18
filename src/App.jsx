e4e2186a71fe1ac387721ef833bb720590dd1a35
Morozov Ivan <morozoff.one@yandex.ru>
2025-01-18 16:18:10 +0300

    added 2nd components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import Header from './components/Header/Header';
+import {CORE_CONCEPTS} from './data.js';
+import CoreConcepts from './components/CoreConcepts/CoreConcepts';
 
 function App() {
   return (
     <div>
       <Header/>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts 
+            title = {CORE_CONCEPTS[0].title}
+            image = {CORE_CONCEPTS[0].image}
+            description = {CORE_CONCEPTS[0].description}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[1]}/>
+            <CoreConcepts {...CORE_CONCEPTS[2]}/>
+            <CoreConcepts {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
       </main>
     </div>
   );
