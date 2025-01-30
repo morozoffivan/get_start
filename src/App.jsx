212bb887d44dc79fe8d90541ea35bc3860747e16
Morozov Ivan <morozoff.one@yandex.ru>
2025-01-30 18:27:09 +0300

    added if statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,83 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Header from './components/Header/Header';
-import {CORE_CONCEPTS} from './data.js';
-import { EXAMPLES } from './data.js';
-import CoreConcepts from './components/CoreConcepts/CoreConcepts';
-import TabButton from './components/TabButton.jsx';
+import Header from "./components/Header/Header";
+import { CORE_CONCEPTS } from "./data.js";
+import { EXAMPLES } from "./data.js";
+import CoreConcepts from "./components/CoreConcepts/CoreConcepts";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState();
 
-  const handleSelect = (selectedBtn) =>{
+  const handleSelect = (selectedBtn) => {
     setSelectedTopic(selectedBtn);
+  };
+
+  let tabContent = <p>Please select a topic</p>;
+
+  if(selectedTopic) {
+    tabContent = (<div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>)
   }
 
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcepts 
-            title = {CORE_CONCEPTS[0].title}
-            image = {CORE_CONCEPTS[0].image}
-            description = {CORE_CONCEPTS[0].description}
+            <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              image={CORE_CONCEPTS[0].image}
+              description={CORE_CONCEPTS[0].description}
             />
-            <CoreConcepts {...CORE_CONCEPTS[1]}/>
-            <CoreConcepts {...CORE_CONCEPTS[2]}/>
-            <CoreConcepts {...CORE_CONCEPTS[3]}/>
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <section id="examples">
-        <h2>Examples</h2>
+          <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={()=>handleSelect('components')}>Component</TabButton>
-            <TabButton onSelect={()=>handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={()=>handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Component
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          <div className="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+
+          {/* {!selectedTopic ? (
+            <p>Please select a topic</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )} */}
+          {/* {//трюк с логикой "и"
+          !selectedTopic && (
+            <p>Please select a topic</p>
+          )}
+          {selectedTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )} */}
+          {tabContent}
         </section>
       </main>
     </div>
